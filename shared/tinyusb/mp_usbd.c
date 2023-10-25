6c1d1f5c8d1adfcf585f64f5c5a8d0c19f9cd088
Angus Gratton <angus@redyak.com.au>
2023-10-25 13:38:36 +1100

    WIP: ports/rp2: Toggle IOs for USB task handling & DCD events.

--- shared/tinyusb/mp_usbd.c
+++ shared/tinyusb/mp_usbd.c
@@ -36,8 +36,28 @@
 #include "device/usbd_pvt.h"
 #endif
 
+#include "pico/stdlib.h"
+
+#define IO_DBG1 2
+#define IO_DBG2 3
+
+static void TU_ATTR_FAST_FUNC _toggle_dbg2(void) {
+    gpio_put(IO_DBG2, !gpio_get(IO_DBG2));
+}
+
 void usbd_task(void) {
+    static bool init;
+    if (MP_UNLIKELY(!init)) {
+        gpio_init(IO_DBG1);
+        gpio_set_dir(IO_DBG1, GPIO_OUT);
+        gpio_init(IO_DBG2);
+        gpio_set_dir(IO_DBG2, GPIO_OUT);
+        irq_add_shared_handler(USBCTRL_IRQ, _toggle_dbg2, PICO_SHARED_IRQ_HANDLER_HIGHEST_ORDER_PRIORITY);
+        init = true;
+    }
+    gpio_put(IO_DBG1, 1);
     tud_task_ext(0, false);
+    gpio_put(IO_DBG1, 0);
 }
 
 #endif
