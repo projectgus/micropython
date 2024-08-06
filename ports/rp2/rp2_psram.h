916f600a0c23ccfe2d5551d020a9d459e6203f31
Phil Howard <phil@gadgetoid.com>
2024-08-06 19:49:40 +0100

    ports/rp2: PSRAM support.
    Add PSRAM support with auto detection.
    
    Performs a best-effort attempt to detect attached PSRAM,
    configure it and *add* it to the MicroPython heap.
    
    If PSRAM is not present, should fall back to use internal
    RAM.
    
    Introduce two new port/board defines:
    
    * MICROPY_HW_ENABLE_PSRAM to enable PSRAM.
    * MICROPY_HW_PSRAM_CS_PIN to define the chip-select pin.
    
    Changes:
    
    ports/rp2/rp2_psram.c/h: Add new PSRAM module.
    ports/rp2/main.c: Add optional PSRAM support.
    ports/rp2/CMakeLists.txt: Include rp2_psram.c.
    ports/rp2/mpconfigport.h: Add MICROPY_HW_ENABLE_PSRAM.
    ports/rp2/modmachine.c: Reconfigure flash on freq change.
    
    Co-authored-by: Kirk Benell <kirk.benell@sparkfun.com>
    Co-authored-by: Mike Bell <mike@mercuna.com>
    Signed-off-by: Phil Howard <phil@gadgetoid.com>

---
+++ ports/rp2/rp2_psram.h
@@ -0,0 +1,38 @@
+/*
+ * This file is part of the MicroPython project, http://micropython.org/
+ *
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2025 Phil Howard
+ *                    Mike Bell
+ *                    Kirk D. Benell
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#include "pico/stdlib.h"
+
+#ifndef MICROPY_INCLUDED_RP2_MACHINE_PSRAM_H
+#define MICROPY_INCLUDED_RP2_MACHINE_PSRAM_H
+
+#define PSRAM_LOCATION _u(0x11000000)
+
+extern size_t psram_init(uint cs_pin);
+
+#endif
